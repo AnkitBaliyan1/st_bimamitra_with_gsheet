587c60ae65fda15fda2af81337c74b71bcb1b1a2
Ankit Baliyan <86275544+AnkitBaliyan1@users.noreply.github.com>
2024-02-26 13:01:17 +0530

    Create bimamitra_db.py

---
+++ bimamitra_db.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+from datetime import date, datetime
+
+def udpate_database(user, query, response, rating="NA"):    
+    # with st.spinner("in progress"):
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    worksheet = "bimamitra"
+
+    existing_data = conn.read(worksheet=worksheet, usecols = list(range(5)), ttl=5)
+
+    today_date = date.today().strftime("%Y-%m-%d")
+    time_now = datetime.now().strftime("%H:%M:%S")
+
+    tem_df = pd.DataFrame(
+        {
+            "date": [today_date],
+            "time": [time_now],
+            "username": [user],
+            "query": [query],
+            "response": [response],
+            "rating": [rating],
+        }
+    )
+
+    tem_df.columns = existing_data.columns
+
+    updated_data = pd.concat([tem_df,existing_data], ignore_index=True)
+
+    conn.update(worksheet=worksheet, data=updated_data)
+
+    return("Successfully updated")
+
