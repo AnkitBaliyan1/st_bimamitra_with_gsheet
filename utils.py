ce5d4460754830c8c3ad0ba23276ec888b3c65fd
Ankit Baliyan <86275544+AnkitBaliyan1@users.noreply.github.com>
2024-09-04 22:01:26 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -19,7 +19,7 @@ def generate_response_rag(user_input):
     api_key=os.environ.get("PINECONE_API_KEY")
     )
     # index = Pinecone.from_existing_index(pinecone_index_name, embeddings, namespace=os.environ.get("NAMESPACE"))
-    index = PineconeVectorStore(index_name=pinecone_index_name, embeddings=embeddings, namespace = os.environ.get("NAMESPACE"), pinecone_api_key= os.environ.get("PINECONE_API_KEY") )
+    index = PineconeVectorStore(index_name=pinecone_index_name, embedding=embeddings, namespace = os.environ.get("NAMESPACE"), pinecone_api_key= os.environ.get("PINECONE_API_KEY") )
 
     similar_doc = index.similarity_search_with_score(user_input, int(k))
 
